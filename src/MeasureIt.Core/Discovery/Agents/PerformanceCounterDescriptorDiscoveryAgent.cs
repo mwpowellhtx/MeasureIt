ba54d09368a3d3bf6c9348e1517c37a786ea8270
Michael W Powell <mwpowellhtx@gmail.com>
2016-10-25 13:30:51 -0400

    added unit tests to verify the equality comparison functionality

--- src/MeasureIt.Core/Discovery/Agents/PerformanceCounterDescriptorDiscoveryAgent.cs
+++ src/MeasureIt.Core/Discovery/Agents/PerformanceCounterDescriptorDiscoveryAgent.cs
@@ -16,50 +16,36 @@ namespace MeasureIt.Discovery.Agents
         {
         }
 
-        private static void AddOrReplace(IList<IPerformanceCounterDescriptor> descriptors,
-            IPerformanceCounterDescriptor descriptor)
+        private static IEnumerable<IPerformanceCounterDescriptor> DiscoverValues(
+            InstrumentationDiscovererOptions options, Type type)
         {
-            // Unable to consider a Descriptor under these conditions.
-            if (descriptor == null
-                || descriptor.Method == null
-                || descriptor.Method.DeclaringType == null)
+            const bool inherited = false;
+
+            var o = options;
+
+            var baseDescriptors = (type.BaseType != null
+                ? DiscoverValues(o, type.BaseType)
+                : new List<IPerformanceCounterDescriptor>()).ToArray();
+
+            var potentialDescriptors = type.GetMethods(o.MethodBindingAttr)
+                .Where(m => m.HasAttributes<PerformanceCounterAttribute>(inherited))
+                .SelectMany(method => method.GetAttributeValues(
+                    (PerformanceCounterAttribute a) => a.Descriptor).Select(d =>
+                    {
+                        d.Method = method;
+                        return d;
+                    })).ToArray();
+
+            // Keep all of our Ps, Ds, and Qs in proper working order.
+            foreach (var d in baseDescriptors)
             {
-                const string message = "Unable to consider descriptor";
-                throw new ArgumentException(message, "descriptor");
+                if (potentialDescriptors.Any(p => p.Equals(d))) continue;
+                yield return d;
             }
 
-            var existing = descriptors.FirstOrDefault(
-                d => d.CategoryType == descriptor.CategoryType
-                     && d.AdapterType == descriptor.AdapterType
-                     && d.Method.GetBaseDefinition() == descriptor.Method.GetBaseDefinition());
-
-            if (existing == null)
-            {
-                descriptors.Add(descriptor);
-                return;
-            }
-
-            if (existing.Method == null || existing.Method.DeclaringType == null)
-            {
-                const string message = "Unable to consider descriptors";
-                throw new ArgumentException(message, "descriptors");
-            }
-
-            // No relation to the existing Method one way or another.
-            if (!(existing.Method.DeclaringType.IsAssignableFrom(descriptor.Method.DeclaringType)
-                  || descriptor.Method.DeclaringType.IsAssignableFrom(existing.Method.DeclaringType)))
-            {
-            }
-
-            if (!descriptor.Method.DeclaringType.IsSubclassOf(existing.Method.DeclaringType))
-            {
-                return;
-            }
-
-            var i = descriptors.IndexOf(existing);
-
-            // Replace the existing Descriptor with one that would supersede it.
-            descriptors[i] = descriptor;
+            // Then we may return the Potential Descriptors.
+            foreach (var p in potentialDescriptors)
+                yield return p;
         }
 
         protected override IEnumerable<IPerformanceCounterDescriptor> DiscoverValues(
@@ -68,39 +54,11 @@ namespace MeasureIt.Discovery.Agents
             var o = options;
 
             // ReSharper disable once PossibleMultipleEnumeration
-            var concreteClassTypes = exportedTypes.Where(
-                type => type.IsClass && !type.IsAbstract).ToArray();
-
-            var decoratedMethods = concreteClassTypes.SelectMany(
-                type => type.GetMethods(o.MethodBindingAttr).Where(
-                    method => method.HasAttributes<PerformanceCounterAttribute>(o.IncludeInherited))).ToArray();
-
-            // TODO: TBD: unique "key" is: CategoryType, AdapterType
-            var potentialDescriptors = decoratedMethods.SelectMany(
-                method => method.GetCustomAttributes<PerformanceCounterAttribute>(o.IncludeInherited)
-                    .Select(a =>
-                    {
-                        a.Descriptor.Method = method;
-                        return a.Descriptor;
-                    }))
-                .ToArray();
-
-            Func<IPerformanceCounterDescriptor, bool> hasMethod = d
-                => !(d.Method == null || d.Method.ReflectedType == null
-                     || d.Method.DeclaringType == null);
-
-            // ReSharper disable PossibleNullReferenceException, AssignNullToNotNullAttribute
-            var descriptors = potentialDescriptors.Filter((items, x)
-                => hasMethod(x) && items.Any(i
-                    => hasMethod(i)
-                       && x.Method.ReflectedType == i.Method.ReflectedType
-                       && x.Method.GetBaseDefinition() == i.Method.GetBaseDefinition()
-                       && x.Method.DeclaringType != i.Method.DeclaringType
-                       && x.Method.DeclaringType.IsSubclassOf(i.Method.DeclaringType)
-                    )
-                ).ToArray();
-
-            return descriptors;
+            foreach (var d in exportedTypes.Where(t => t.IsClass && !t.IsAbstract)
+                .SelectMany(t => DiscoverValues(o, t)))
+            {
+                yield return d;
+            }
         }
     }
 }
