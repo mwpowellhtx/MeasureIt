a07123e2cda2525fb03ce547da0456a9451d65ae
Michael W Powell <mwpowellhtx@gmail.com>
2016-10-26 11:58:14 -0400

    added root type to measurement options

--- src/MeasureIt.Core/Descriptors/IMeasurementOptions.cs
+++ src/MeasureIt.Core/Descriptors/IMeasurementOptions.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 namespace MeasureIt
 {
@@ -7,6 +8,12 @@ namespace MeasureIt
     /// </summary>
     public interface IMeasurementOptions : IPublishingOptions, ISamplingOptions
     {
+        /// <summary>
+        /// Gets or sets the RootType. Due to the strategy in terms of how
+        /// </summary>
+        Type RootType { get; set; }
+
+        // TODO: TBD: may need/want to reconsider how this is actually discovered, beyond the recursive method (?)
         /// <summary>
         /// 
         /// </summary>
