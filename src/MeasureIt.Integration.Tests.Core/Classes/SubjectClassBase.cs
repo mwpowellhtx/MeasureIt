bbfd918af394ecfdaf5de4d1cb9e65a3812eae1a
Michael W Powell <mwpowellhtx@gmail.com>
2016-11-21 11:04:10 -0500

    added core integration unit test concerns anticipating will serve at least castle windsor and autofac for starters

---
+++ src/MeasureIt.Integration.Tests.Core/Classes/SubjectClassBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace MeasureIt
+{
+    /// <summary>
+    /// The same rules apply utilizing Dynamic Proxy here as with any other proxy usage. Members
+    /// must be public and virtual, or protected internal, in order to be interception eligible.
+    /// </summary>
+    public abstract class SubjectClassBase
+    {
+        [MeasurePerformance(
+            typeof(IntegrationPerformanceCounterCategoryAdapter)
+            , typeof(TotalMemberAccessesPerformanceCounterAdapter)
+            , InstanceLifetime = PerformanceCounterInstanceLifetime.Process
+            , ThrowPublishErrors = true
+            )]
+        public virtual void Verify()
+        {
+        }
+
+        public virtual void DoesNotVerify()
+        {
+        }
+    }
+}
---
+++ src/MeasureIt.Integration.Tests.Core/Counters/WindsorPerformanceCounterCategoryAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace MeasureIt
+{
+    public class IntegrationPerformanceCounterCategoryAdapter : PerformanceCounterCategoryAdapterBase
+    {
+        public IntegrationPerformanceCounterCategoryAdapter()
+            : base("Integration", "Integration performance counter category")
+        {
+        }
+    }
+}
