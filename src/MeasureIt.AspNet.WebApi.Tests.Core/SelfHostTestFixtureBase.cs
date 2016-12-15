06ce687bbb58cf39ab25e3f90ab2cdde503d1f3c
Michael W Powell <mwpowellhtx@gmail.com>
2016-12-15 16:47:30 -0500

    updated self host test fixture base class

--- src/MeasureIt.AspNet.WebApi.Tests.Core/SelfHostTestFixtureBase.cs
+++ src/MeasureIt.AspNet.WebApi.Tests.Core/SelfHostTestFixtureBase.cs
@@ -9,21 +9,54 @@ namespace MeasureIt
     public abstract class SelfHostTestFixtureBase<TStartup> : DisposableTestFixtureBase
         where TStartup : Startup
     {
+        protected static string GetUrl()
+        {
+            // TODO: TBD: completely arbitrary port number(s)...
+            var port = new Random().Next(9000, 10000 - 1);
+            return string.Format("http://localhost:{0}/", port);
+        }
+
         private readonly IDisposable _webApp;
 
         private readonly string _url;
 
-        protected SelfHostTestFixtureBase(string url)
+        private string _baseApiUrl;
+
+        /// <summary>
+        /// Gets the BaseApiUrl.
+        /// </summary>
+        protected string BaseApiUrl
         {
+            get { return _baseApiUrl; }
+            private set
+            {
+                var s = value;
+                Assert.NotNull(s);
+                Assert.NotEqual(string.Empty, s);
+                Assert.StartsWith("api/", s);
+                _baseApiUrl = s;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="url">The self hosted web server Url.</param>
+        /// <param name="baseApiUrl">The base API address.</param>
+        protected SelfHostTestFixtureBase(string url, string baseApiUrl)
+        {
+            BaseApiUrl = baseApiUrl;
             _url = url;
             // TODO: TBD: may need/want the Options ctor...
             _webApp = WebApp.Start<TStartup>(url);
             Assert.NotNull(_webApp);
         }
 
+        public delegate void ResponseHandler(HttpResponseMessage response);
+
         protected interface IHttpResponseHandler
         {
-            void Handle(Action<HttpResponseMessage> handler);
+            void Handle(ResponseHandler handler);
         }
 
         protected class HttpResponseHandler : IHttpResponseHandler
@@ -35,7 +68,7 @@ namespace MeasureIt
                 _response = response;
             }
 
-            public void Handle(Action<HttpResponseMessage> handler)
+            public void Handle(ResponseHandler handler)
             {
                 Assert.NotNull(handler);
                 handler(_response);
@@ -48,6 +81,7 @@ namespace MeasureIt
 
             using (var client = new HttpClient {BaseAddress = new Uri(_url)})
             {
+                // TODO: TBD: don't know how to get at the request, per se...
                 return new HttpResponseHandler(action(client));
             }
         }
