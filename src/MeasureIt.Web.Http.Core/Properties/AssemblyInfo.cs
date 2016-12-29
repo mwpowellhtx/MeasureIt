aeb79c66f0fed2feb11eff4207a7b6106544dbf2
Michael W Powell <mwpowellhtx@gmail.com>
2016-12-29 18:18:14 -0500

    updated packaging identifiers a bit

--- src/MeasureIt.Web.Http.Core/Properties/AssemblyInfo.cs
+++ src/MeasureIt.Web.Http.Core/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MeasureIt.Web.Http.Core")]
+[assembly: AssemblyTitle("MeasureIt.AspNet.WebApi.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("MeasureIt.Web.Http")]
+[assembly: AssemblyProduct("MeasureIt.AspNet.WebApi")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3.*")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.4.*")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
 
 // NuGet NuSpec Token Replacement should work, but is not: $version$
 // TODO: TBD: https://github.com/NuGet/Home/issues/4149
