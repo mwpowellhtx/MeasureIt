caf459e65767d613b4a24281d85d9b1996a37989
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-04 15:03:15 -0500

    updated package references updated dependency injection within project

--- src/MeasureIt.AspNet.Mvc.Castle.Windsor.Website/Global.asax.cs
+++ src/MeasureIt.AspNet.Mvc.Castle.Windsor.Website/Global.asax.cs
@@ -5,8 +5,19 @@ using System.Web.Routing;
 
 namespace MeasureIt.AspNet.Mvc.Castle.Windsor
 {
+    using Controllers;
+    using Kingdom.Web.Mvc;
+    using global::Castle.Windsor;
+
     public class MvcApplication : HttpApplication
     {
+        private IWindsorContainer Container { get; }
+
+        public MvcApplication()
+        {
+            Container = new WindsorContainer();
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -15,6 +26,10 @@ namespace MeasureIt.AspNet.Mvc.Castle.Windsor
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+            // Assumes filters, routes, bundles, etc, have all been configured.
+            Container.InstallMvcServices<HomeController>()
+                .UseDependencyResolver()
+                ;
         }
     }
 }
