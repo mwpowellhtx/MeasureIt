263ecb5c4b7e54e12730857c4139ace437ac0882
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-12 12:18:40 -0500

    added documentation

--- src/MeasureIt.Core/Descriptors/IPublishingOptions.cs
+++ src/MeasureIt.Core/Descriptors/IPublishingOptions.cs
@@ -1,27 +1,27 @@
 ﻿namespace MeasureIt
 {
     /// <summary>
-    /// 
+    /// Publishing options.
     /// </summary>
     public interface IPublishingOptions
     {
         /// <summary>
-        /// 
+        /// Gets or sets whether to PublishCounters.
         /// </summary>
         bool PublishCounters { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets whether to ThrowPublishErrors.
         /// </summary>
         bool ThrowPublishErrors { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets whether to PublishEvent.
         /// </summary>
         bool PublishEvent { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets whether MayProceed with Measurement Unabated.
         /// </summary>
         bool MayProceedUnabated { get; }
     }
