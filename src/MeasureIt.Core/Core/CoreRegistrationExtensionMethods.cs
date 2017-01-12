18e89dcd819af47a5e158d3236da41b4ecd90493
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-12 12:46:24 -0500

    did language updates added verification of class type

--- src/MeasureIt.Core/Core/CoreRegistrationExtensionMethods.cs
+++ src/MeasureIt.Core/Core/CoreRegistrationExtensionMethods.cs
@@ -16,7 +16,21 @@ namespace MeasureIt
             // Rule out whether TInterface is indeed an interface.
             if (type.IsInterface) return;
 
-            var message = string.Format("Requires an Interface type instead of {0}", type.FullName);
+            var message = $"Requires an Interface type instead of {type.FullName}";
+
+            throw new InvalidOperationException(message);
+        }
+
+        /// <summary>
+        /// Verifies whether <paramref name="type"/> <see cref="Type.IsClass"/>.
+        /// </summary>
+        /// <param name="type"></param>
+        public static void VerifyIsClass(this Type type)
+        {
+            // Rule out whether TInterface is indeed an interface.
+            if (type.IsClass) return;
+
+            var message = $"Requires an Class type instead of {type.FullName}";
 
             throw new InvalidOperationException(message);
         }
