b7a4c0a00a504aabb4e92ba7159f2205f4e08577
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-12 13:08:37 -0500

    updated global installer contexts

--- src/MeasureIt.AspNet.Mvc.Castle.Windsor.Website/Global.asax.cs
+++ src/MeasureIt.AspNet.Mvc.Castle.Windsor.Website/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -6,7 +10,10 @@ using System.Web.Routing;
 namespace MeasureIt.AspNet.Mvc.Castle.Windsor
 {
     using Controllers;
+    using Discovery;
     using Kingdom.Web.Mvc;
+    using Web.Mvc.Castle.Windsor;
+    using Web.Mvc.Discovery;
     using global::Castle.Windsor;
 
     public class MvcApplication : HttpApplication
@@ -16,6 +23,21 @@ namespace MeasureIt.AspNet.Mvc.Castle.Windsor
         public MvcApplication()
         {
             Container = new WindsorContainer();
+        }
+
+        private static IEnumerable<Assembly> GetMeasurementAssemblies()
+        {
+            yield return typeof(MvcApplication).Assembly;
+        }
+
+        private static InstrumentationDiscoveryOptions CreateOptions()
+        {
+            return new InstrumentationDiscoveryOptions
+            {
+                ThrowOnInstallerFailure = false,
+                ThrowOnUninstallerFailure = false,
+                Assemblies = GetMeasurementAssemblies().ToArray()
+            };
         }
 
         protected void Application_Start()
@@ -29,7 +51,25 @@ namespace MeasureIt.AspNet.Mvc.Castle.Windsor
             // Assumes filters, routes, bundles, etc, have all been configured.
             Container.InstallMvcServices<HomeController>()
                 .UseDependencyResolver()
+                .EnableMvcMeasurements<
+                    IMvcActionInstrumentationDiscoveryService
+                    , MvcActionInstrumentationDiscoveryService
+                    , InstrumentationDiscoveryOptions>(CreateOptions)
                 ;
+
+            var kernel = Container.Kernel;
+
+            Debug.Assert(kernel.HasComponent(typeof(IDependencyResolver)));
+            Debug.Assert(kernel.HasComponent(typeof(IWindsorDependencyResolver)));
+            Debug.Assert(kernel.HasComponent(typeof(IActionInvoker)));
+
+            // TODO: TBD: need to connect with startup? warmup? Initialize the performance counters, install them... but don't forget to also uninstall them?
+
+            using (var context = Container.Resolve<IInstallerInstrumentationDiscoveryService>()
+                .GetInstallerContext())
+            {
+                context.Install();
+            }
         }
     }
 }
