240a4e50f49ca7070284dd0121078b1b77780988
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-12 13:05:37 -0500

    updated installer context added discovery options did language updates

--- src/MeasureIt.Core/Contexts/InstallerContext.cs
+++ src/MeasureIt.Core/Contexts/InstallerContext.cs
@@ -15,24 +15,21 @@ namespace MeasureIt.Contexts
     /// </summary>
     public class InstallerContext : ContextBase, IInstallerContext
     {
-        private IInstrumentationDiscoveryOptions Options { get; set; }
+        private IInstrumentationDiscoveryOptions DiscoveryOptions { get; }
 
         private readonly Lazy<IInstallerInstrumentationDiscoveryService> _lazyDiscoveryService;
 
-        private IInstallerInstrumentationDiscoveryService Service
-        {
-            get { return _lazyDiscoveryService.Value; }
-        }
+        private IInstallerInstrumentationDiscoveryService Service => _lazyDiscoveryService.Value;
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="options"></param>
+        /// <param name="discoveryOptions"></param>
         /// <param name="discoveryService"></param>
-        public InstallerContext(IInstrumentationDiscoveryOptions options
+        public InstallerContext(IInstrumentationDiscoveryOptions discoveryOptions
             , IInstallerInstrumentationDiscoveryService discoveryService)
         {
-            Options = options;
+            DiscoveryOptions = discoveryOptions;
 
             const LazyThreadSafetyMode execAndPubThreadSafety = LazyThreadSafetyMode.ExecutionAndPublication;
 
@@ -54,9 +51,12 @@ namespace MeasureIt.Contexts
             // TODO: TBD: may not need this context quite as much as "simply" the extension methods...
             Uninstall();
 
-            var throwOnInstallerFailure = Options.ThrowOnInstallerFailure;
+            var o = DiscoveryOptions;
 
-            using (var adapter = new PerformanceCounterCategoryInstallerContextAdapter(Service.CategoryAdapters.Values))
+            var throwOnInstallerFailure = o.ThrowOnInstallerFailure;
+
+            using (var adapter = new PerformanceCounterCategoryInstallerContextAdapter(
+                o, Service.CategoryAdapters.Values))
             {
                 foreach (var tuple in adapter.GetInstalledCategories())
                 {
@@ -81,25 +81,41 @@ namespace MeasureIt.Contexts
             return Task.Run(() => Install());
         }
 
+        private class UninstallState
+        {
+            /// <summary>
+            /// Gets or sets the Result of the <see cref="Uninstall"/> operation.
+            /// </summary>
+            internal bool Result { get; set; }
+
+            /// <summary>
+            /// Gets or sets the Name of the <see cref="Uninstall"/> state.
+            /// </summary>
+            internal string Name { get; set; }
+        }
+
         /// <summary>
         /// Uninstalls the Performance Counter Categories.
         /// </summary>
         public virtual void Uninstall()
         {
-            var throwOnInstallerFailure = Options.ThrowOnInstallerFailure;
+            var o = DiscoveryOptions;
+
+            var throwOnUninstallerFailure = o.ThrowOnUninstallerFailure;
 
             // TODO: TBD: ditto Install re: extension methods...
-            using (var adapter = new PerformanceCounterCategoryUninstallerContextAdapter(Service.CategoryAdapters.Values))
+            using (var adapter = new PerformanceCounterCategoryUninstallerContextAdapter(
+                o, Service.CategoryAdapters.Values))
             {
                 IEnumerable<string> categoryNames;
 
                 var results = adapter.TryUninstallCategories(out categoryNames);
 
                 var unable = results.Zip(categoryNames,
-                    (result, name) => new {Result = result.Item2, Name = name})
+                        (result, name) => new UninstallState {Result = result.Item2, Name = name})
                     .Where(z => !z.Result).ToArray();
 
-                if (!unable.Any() || !throwOnInstallerFailure) return;
+                if (!unable.Any() || !throwOnUninstallerFailure) return;
 
                 // Ditto C# 6.0 language features.
                 var message = $"Unable to uninstall the {typeof(PerformanceCounterCategory)} '{string.Join(", ", unable.Select(x => x.Name))}' definitions.";
