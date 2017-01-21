39c3a4fee1893f01b4bd75a3ee7159b1285fd34c
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-21 16:26:20 -0500

    added or updated test web site

--- src/MeasureIt.AspNet.Mvc.Autofac.Website/Global.asax.cs
+++ src/MeasureIt.AspNet.Mvc.Autofac.Website/Global.asax.cs
@@ -1,18 +1,80 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MeasureIt.AspNet.Mvc.Autofac.Controllers;
 
 namespace MeasureIt.AspNet.Mvc.Autofac
 {
+    using Discovery;
+    using Web.Mvc.Autofac;
+    using global::Autofac;
+    using global::Autofac.Integration.Mvc;
+
     public class MvcApplication : HttpApplication
     {
+        private ContainerBuilder Builder { get; }
+
+        public MvcApplication()
+        {
+            Builder = new ContainerBuilder();
+        }
+
+        private static IEnumerable<Assembly> GetMeasurementAssemblies()
+        {
+            yield return typeof(MvcApplication).Assembly;
+        }
+
+        private static MvcInstrumentationDiscoveryOptions CreateOptions()
+        {
+            return new MvcInstrumentationDiscoveryOptions
+            {
+                ThrowOnInstallerFailure = false,
+                ThrowOnUninstallerFailure = false,
+                Assemblies = GetMeasurementAssemblies().ToArray()
+            };
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var builder = Builder;
+
+            // Assumes filters, routes, bundles, etc, have all been configured.
+
+            // TODO: TBD: could/should potentially put these in a useful extension method...
+            builder.RegisterType<AutofacControllerActionInvoker>().As<IActionInvoker>().InstancePerRequest();
+            builder.RegisterType<AutofacDependencyResolver>().AsImplementedInterfaces().SingleInstance();
+
+            builder.RegisterControllers(typeof(HomeController).Assembly).InjectActionInvoker();
+
+            builder.EnableMvcMeasurements<
+                    IMvcActionInstrumentationDiscoveryService
+                    , MvcActionInstrumentationDiscoveryService
+                    , MvcInstrumentationDiscoveryOptions>(CreateOptions)
+                ;
+
+            // Build the Container and Resolve the Resolver.
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(container.Resolve<IDependencyResolver>());
+
+            /* Obviously, in a more production oriented environment, we would not necessarily want
+             * to install the Measurement Context like this. */
+
+            using (var context = container.Resolve<IInstallerInstrumentationDiscoveryService>()
+                .GetInstallerContext())
+            {
+                context.Install();
+            }
         }
     }
 }
