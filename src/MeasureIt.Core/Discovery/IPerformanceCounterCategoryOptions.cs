78acccd07a15c7157ff88c269f924b5555e4e3ed
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-21 16:24:21 -0500

    added options but will leave them commented out for now

---
+++ src/MeasureIt.Core/Discovery/IPerformanceCounterCategoryOptions.cs
@@ -0,0 +1,15 @@
+﻿//using System.Diagnostics;
+
+//namespace MeasureIt.Discovery
+//{
+//    /// <summary>
+//    /// Establishes some <see cref="PerformanceCounterCategory"/> options.
+//    /// </summary>
+//    public interface IPerformanceCounterCategoryOptions
+//    {
+//        /// <summary>
+//        /// Gets or sets whether to PrepareCategoryName.
+//        /// </summary>
+//        bool PrepareCategoryName { get; set; }
+//    }
+//}
