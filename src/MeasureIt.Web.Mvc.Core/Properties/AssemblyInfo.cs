43f376e983b7b683c4c7a9a4d281d1369ba9307d
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-22 15:25:52 -0500

    this was a non trivial commit sorting through all the versions and packaging did finally bump what was deemed necessary for packaging and assembly versions

--- src/MeasureIt.Web.Mvc.Core/Properties/AssemblyInfo.cs
+++ src/MeasureIt.Web.Mvc.Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MeasureIt.AspNet.Mvc")]
+[assembly: AssemblyTitle("MeasureIt.AspNet.Mvc.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("MeasureIt.AspNet.Mvc")]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.*")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.2.2.*")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
