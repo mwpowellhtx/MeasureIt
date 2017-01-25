aaf28089070f408039ddf934e1a5062e2cbd3a80
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-24 21:19:04 -0500

    bumped package and assembly versions

--- src/MeasureIt.Web.Http.Core/Properties/AssemblyInfo.cs
+++ src/MeasureIt.Web.Http.Core/Properties/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.*")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.3.0.*")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 // NuGet NuSpec Token Replacement should work, but is not: $version$
 // TODO: TBD: https://github.com/NuGet/Home/issues/4149
